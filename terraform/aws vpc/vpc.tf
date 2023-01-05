5ac5eb66fe8815053f906dde40eff1d89f11044e
노가성 <82383294+NohGaSeong@users.noreply.github.com>
2023-01-05 23:11:01 +0900

    nat 게이트웨이 추가

--- terraform/aws vpc/vpc.tf
+++ terraform/aws vpc/vpc.tf
@@ -78,3 +78,47 @@ resource "aws_route_table_association" "public_rt_association_2" {
   subnet_id = aws_subnet.public_subnet2.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+# EIP 생성
+resource "aws_eip" "nat_ip" {
+  vpc = true
+  
+  lifecycle {
+    cretae_before_destroy = true
+  }
+}
+
+# NAT 게이트웨이
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_ip.id
+  
+  subnet_id = aws_subnet.public_subnet1.id
+  
+  tags = {
+    Name = "public-nat"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.simple_vpc.id
+  
+  tags = {
+    Name = "private_rt"
+  }
+}
+
+# private rtb에 연결
+resource "aws_route_table_association" "private_rt_association1" {
+  subnet_id = aws_subnet.private_subnet1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_association2" {
+  subnet_id = aws_subnet.private_subnet2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route" "private_rt_nat" {
+  route_table_id = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat_gateway.id
